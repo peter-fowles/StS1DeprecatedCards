2aac439e038ff89e058d7e8baa3689726f66694c
Peter Fowles <foul.in.basket@gmail.com>
2026-04-17 12:05:20 -0600

    Initial Commit

---
+++ src/MainFile.cs
@@ -0,0 +1,23 @@
+using Godot;
+using HarmonyLib;
+using MegaCrit.Sts2.Core.Modding;
+
+namespace StS1DeprecatedThings;
+
+//You're recommended but not required to keep all your code in this package and all your assets in the StS1DeprecatedThings folder.
+[ModInitializer(nameof(Initialize))]
+public partial class MainFile : Node
+{
+    public const string
+        ModId = "StS1DeprecatedThings"; //At the moment, this is used only for the Logger and harmony names.
+
+    public static MegaCrit.Sts2.Core.Logging.Logger Logger { get; } =
+        new(ModId, MegaCrit.Sts2.Core.Logging.LogType.Generic);
+
+    public static void Initialize()
+    {
+        Harmony harmony = new(ModId);
+
+        harmony.PatchAll();
+    }
+}
