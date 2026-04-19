749c9e4a2d5e78d2dbb1f42a719ffcc4fcf5d791
Peter Fowles <foul.in.basket@gmail.com>
2026-04-18 21:24:56 -0600

    Added Reaper

---
+++ StS1DeprecatedThingsCode/Cards/Reaper.cs
@@ -0,0 +1,47 @@
+using BaseLib.Abstracts;
+using BaseLib.Cards.Variables;
+using BaseLib.Utils;
+using MegaCrit.Sts2.Core.Commands;
+using MegaCrit.Sts2.Core.Commands.Builders;
+using MegaCrit.Sts2.Core.Entities.Cards;
+using MegaCrit.Sts2.Core.Entities.Creatures;
+using MegaCrit.Sts2.Core.GameActions.Multiplayer;
+using MegaCrit.Sts2.Core.Localization.DynamicVars;
+using MegaCrit.Sts2.Core.Models.CardPools;
+using MegaCrit.Sts2.Core.ValueProps;
+
+namespace StS1DeprecatedThings.StS1DeprecatedThingsCode.Cards;
+
+[Pool(typeof(IroncladCardPool))]
+public class Reaper() : CustomCardModel(2, CardType.Attack,
+    CardRarity.Rare, TargetType.AllEnemies)
+{
+    public override string? CustomPortraitPath => "StS1DeprecatedThings/assets/CardPortraits/reaper.png";
+
+    protected override IEnumerable<DynamicVar> CanonicalVars => [
+        new DamageVar(4, ValueProp.Move),
+    ];
+
+    public override IEnumerable<CardKeyword> CanonicalKeywords => [
+        CardKeyword.Exhaust
+    ];
+
+    protected override async Task OnPlay(
+        PlayerChoiceContext choiceContext,
+        CardPlay play)
+    {
+        Reaper card = this;
+        AttackCommand attack = await DamageCmd.Attack(DynamicVars.Damage.BaseValue)
+            .FromCard(this)
+            .TargetingAllOpponents(CombatState)
+            .WithHitFx("vfx/vfx_attack_blunt", null, "heavy_attack.mp3")
+            .Execute(choiceContext);
+        IEnumerable<DamageResult> damageResults = attack.Results;
+        await CreatureCmd.Heal(card.Owner.Creature, damageResults.Sum(result => result.UnblockedDamage));
+    }
+
+    protected override void OnUpgrade()
+    {
+        DynamicVars.Damage.UpgradeValueBy(1M);
+    }
+}
